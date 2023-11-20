eb4d9d5dc11cb276f665702ce065a8b893b53024
geunhyee <147581668+geunhyee@users.noreply.github.com>
2023-11-20 09:44:50 +0900

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,20 @@
+package ch01_start;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+//		실행 단축기 ctrl + F11
+//		자동완성 단축키 ctrl + Space
+//		코드라인 복사 ctrl + Alt + 방향키 아래
+//		여러줄 주석
+		/*
+		 * 
+		 **/
+		
+	}
+
+}
